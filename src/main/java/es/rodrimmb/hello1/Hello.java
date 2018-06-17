ca3f81496e6335fbe7c57685478a2b3247a30e54
rodrimmb <rodrimmb@gmail.com>
2018-06-17 20:52:09 +0200

    Primer commit

---
+++ src/main/java/es/rodrimmb/hello1/Hello.java
@@ -0,0 +1,22 @@
+package es.rodrimmb.hello1;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+@Named
+@RequestScoped
+public class Hello {
+
+    private String name;
+
+    public Hello() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
